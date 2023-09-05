a39cd551a9911bfdb6c3a92d0d410515367be8b1
Dawid Wołosowicz <d@1823.pl>
2023-09-05 17:01:15 +0200

    show the update date in the page template

--- src/_page.html.php
+++ src/_page.html.php
@@ -1,6 +1,6 @@
 <section class="page">
     <div class="page__header">
-        <?php if (isset($creation_date)): ?><div class="header__meta"><a href="/">&laquo; back</a> &mdash; from <?= e($creation_date->format('d/m/Y')) ?><?php if (!empty($tags)): ?>, tags:
+        <?php if (isset($creation_date)): ?><div class="header__meta"><a href="/">&laquo; back</a> &mdash; from <?= e($creation_date->format('d/m/Y')) ?><?php if ($update_date ?? false): ?>, updated <?= e($update_date->format('d/m/Y')) ?><?php endif; ?><?php if (!empty($tags)): ?>, tags:
                 <?php foreach($tags as $index => $tag): ?>
                     <a href="<?= e($tag->url) ?>"><?= e($tag->label) ?></a><?php echo ($index < count($tags) - 1) ? ', ' : ''; ?>
                 <?php endforeach; ?>
